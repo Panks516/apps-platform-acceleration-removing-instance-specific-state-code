789238e4901305f41b5cabf8fee4028205ba71a1
pankaj.a.khurana <pankaj.a.khurana@accenture.com>
2017-12-14 09:12:27 -0600

    adding-instance

--- start-load-balancer.sh
+++ start-load-balancer.sh
@@ -3,4 +3,5 @@
 [ ! `which nginx` ] && echo "please install nginx - 'brew install nginx'" && exit 1
 
 echo "starting nginx"
-nginx -p support -c nginx.conf
+
+C:/nginx-1.13.7/nginx -p support -c nginx.conf
